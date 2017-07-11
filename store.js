c70acaf24b2fcaff9c4eb82798ea3c787ab26b72
adityab2017 <t-adbis@microsoft.com>
2017-07-11 22:13:13 +0800

    restaurants

--- store.js
+++ store.js
@@ -6,7 +6,26 @@ module.exports = {
 
             // Filling the Restaurant results manually just for demo purposes
             var cuisine = [];
-            for (var i = 1; i <= 5; i++) {
+
+            cuisine.push({
+                name: 'Summer Pavillion',
+                location: destination,
+                rating: Math.ceil(Math.random() * 5),
+                numberOfReviews: Math.floor(Math.random() * 500) + 1,
+                priceStarting: Math.floor(Math.random() * 45) + 80,
+                image: 'https://placeholdit.imgix.net/~text?txtsize=35&txt=Restaurant+1' + '&w=500&h=260'
+            });
+
+            cuisine.push({
+                name: 'Rakuzen',
+                location: destination,
+                rating: Math.ceil(Math.random() * 5),
+                numberOfReviews: Math.floor(Math.random() * 500) + 1,
+                priceStarting: Math.floor(Math.random() * 45) + 80,
+                image: 'https://placeholdit.imgix.net/~text?txtsize=35&txt=Restaurant+2'+ '&w=500&h=260'
+            });
+
+            for (var i = 3; i <= 5; i++) {
                 cuisine.push({
                     name: ' Restaurant ' + i,
                     location: destination,
